0a70cd513bb4d7c99ca1571e50b337404633580b
LeoTheHuman <leothehuman@gmail.com>
2018-12-12 04:20:10 -0800

    README update

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 Quotes cards and rules of the Magic: the Gathering TCG.
 
+# Howto:
+
+$ ./make_data.sh
+$ docker build -t <container-name> .
+$ docker run <container-name>
+
+# Commands
+
 /o <card name or search strings> - oracle text for a card
 
 /q <question> - oracle text for cards mentioned in the question
--- make_data.sh
+++ make_data.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl http://media.wizards.com/2018/downloads/MagicCompRules%2020181005.txt | iconv -f cp1251 -t utf8 > cr.txt
 mkdir -p data
 python3 scripts/update_cards.py
+curl http://media.wizards.com/2018/downloads/MagicCompRules%2020181005.txt | iconv -f cp1251 -t utf8 > cr.txt
 python3 scripts/update_cr.py
